76bec3fa5335a23c13365117edea7125f88c020d
Viacheslav Verchonv <verchonov.viacheslav@gmail.com>
2019-03-04 19:59:37 +0200

    card.js added

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = () => {
+  return(
+    <div>
+      <img src="" alt="robot"/>
+      <div>
+        <h2>
+          VV
+        </h2>
+        <p>vv@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
